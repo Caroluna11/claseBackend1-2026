c951712640d106c8da15dd0feed15476010bb1fa
Carolina Bedoya Rendón <aonixsoul@gmail.com>
2026-02-09 21:21:06 -0500

    fix: us-001 menu product and costumer

--- src/main/java/constumersystem/SaleSystem.java
+++ src/main/java/constumersystem/SaleSystem.java
@@ -11,17 +11,22 @@ public class SaleSystem {
     static double price;
     static int quantity;
     static boolean state;
+    //Costumer attributes
+    static int idCostumer;
+    static String name;
+    static String lastName;
+    static String address;
+    static int phone;
+    static String email;
+
 
     public static void main(String[] args) {
-        createProducts();
-        getProducts();
-        System.out.println("Ingrese el id del producto: ");
-        int id = sc.nextInt();
-        sc.nextLine();
-        updateProducts(id);
-        System.out.println("Ingrese el id del producto");
-        id = sc.nextInt();
-        deleteProducts(id);
+
+        menu();
+
+
+
+
     }
 
     //Class methods
@@ -79,6 +84,171 @@ public class SaleSystem {
             state = false;
         }else {
             System.out.println("Producto" + id + " encontrado");
+        }
+    }
+
+    //Costumer methods
+    public static void createCostumer(){
+        System.out.println("Por favor ingresa tu numero de cedula: ");
+        idCostumer = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el nombre del cliente:  ");
+        name= sc.nextLine();
+        System.out.println("ingrese el apellido del cliente: ");
+        lastName = sc.nextLine();
+        System.out.println("Ingrese la dirección del cliente: ");
+        address = sc.nextLine();
+        System.out.println("Ingrese el telefono del cliente: ");
+        phone = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el correo del cliente: ");
+        email= sc.nextLine();
+    }
+
+    //get costumer
+    public static void getCostumer(){
+        System.out.println("id: " + idCostumer + "\n" +
+                "Nombre: " + name + "\n" +
+                "Apellido: " + lastName + "\n" +
+                "Dirección: " + address + "\n" +
+                "Telefono: " + phone + "\n"+
+                "Correo: " + email + "\n");
+    }
+
+    //Update costumers
+    public static void updateCostumer(int findId) {
+        if (findId == idCostumer) {
+            System.out.println("Ingrese el id del cliente ");
+            idCostumer= sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ingrese el nombre del cliente: ");
+            name = sc.nextLine();
+            System.out.println("ingrese el apellido del cliente: ");
+            lastName= sc.nextLine();
+            System.out.println("Ingrese la dirección del cliente: ");
+            address = sc.nextLine();
+            System.out.println("Ingrese el telefono del cliente: ");
+            phone = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ingrese el email del cliente: ");
+            email= sc.nextLine();
+        } else {
+            System.out.println("Cliente no encontrado");
+        }
+    }
+
+    //delete costumers
+    public static void deleteCostumer(int idDeleteCostu){
+        if (idDeleteCostu == idCostumer) {
+            idCostumer = 0;
+            name = "";
+            lastName = "";
+            address= "";
+            phone= 0;
+            email= "";
+
+        }else {
+            System.out.println("Cliente" + id + "no encontrado");
+        }
+    }
+    public static void menu(){
+        int respuesta=0;
+        int respuestaSubmenu =0;
+        while (true) {
+            System.out.println(""" 
+                    Por favor ingresa la opcion que deseas: 
+                    1. Productos
+                    2. Clientes
+                    3. Ventas
+                    4. Salir """);
+           respuesta= sc.nextInt();
+           sc.nextLine();
+           switch (respuesta){
+               case 1 ->{
+                   bucleExternal:
+                   while (true){
+                       System.out.println("""
+                               Selecciona lo que deseas hacer:
+                               1. Crea un nuevo producto.
+                               2. Ver los productos
+                               3. Actualizar un producto
+                               4. Eliminar un producto
+                               5. Salir""");
+                       respuestaSubmenu= sc.nextInt();
+                       switch (respuestaSubmenu){
+                           case 1 ->{
+                               createProducts();
+                           }
+                           case 2 ->{
+                               getProducts();
+                           }
+                           case 3 ->{
+                               System.out.println("Ingrese el id del producto para actualizar: ");
+                               int id = sc.nextInt();
+                               sc.nextLine();
+                               updateProducts(id);
+                           }
+                           case 4 ->{
+                               System.out.println("Ingrese el id del producto a eliminar");
+                               int id = sc.nextInt();
+                               sc.nextLine();
+                               deleteProducts(id);
+                           }
+                           case 5 ->{
+                               break bucleExternal;
+                           }
+                       }
+
+                   }
+
+               }
+               case 2->{
+                   bucleExternal:
+                   while (true){
+                       System.out.println("""
+                               Selecciona lo que deseas hacer:
+                               1. Crear un nuevo cliente.
+                               2. Ver los clientes.
+                               3. Actualizar un cliente.
+                               4. Eliminar un cliente.
+                               5. Salir""");
+                       respuestaSubmenu= sc.nextInt();
+                       switch (respuestaSubmenu){
+                           case 1 ->{
+                               createCostumer();
+                           }
+                           case 2 ->{
+                               getCostumer();
+                           }
+                           case 3 -> {
+                               System.out.println("Por favor ingrese el id del cliente a actualizar");
+                               int id = sc.nextInt();
+                               sc.nextLine();
+                               updateCostumer(id);
+                           }
+                           case 4 ->{
+                               System.out.println("Por favor ingrese el id del cliente a eliminar");
+                               int id= sc.nextInt();
+                               sc.nextLine();
+                               deleteCostumer(id);
+                           }
+                           case 5 ->{
+                               break bucleExternal;
+                           }
+                       }
+
+                   }
+
+               }
+               case 3 -> {}
+               case 4 -> {
+                   System.exit(0);
+
+               }
+
+           }
+
+
         }
     }
 }
